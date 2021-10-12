1d5a5ffe04b17bc05bf0de4adeccc813d4495a48
EliClaw56 <89602283+EliClaw56@users.noreply.github.com>
2021-10-11 22:10:24 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,22 +11,45 @@ let plants = [{
 }];
 
 let randomIndex;
+let animating = false;
+// let counter = 0;
 
 function setup() {
   createCanvas(400, 400);
-  background(200,50,110);
-  createButton("start").mousePressed();
+  textSize(32);
+  text("click to randomize",50,50);
 }
 
 function draw() {
+  if(animating == true){
+    ellipse(random(width),random(height),random(50,200));
+  }
 }
+// function changeBackground(){
+//   if (counter <= 5){
+//     counter++;
+//     console.log(counter)
+//   background(random(255),random(255), random(255));
+//   setTimeout(changeBackground,1000);
+// } else
+// }
+function randomizer(){
+  animating = false;
+  if (plants[0]) {
+    background(random(255), random(255), random(255));
+    randomIndex = int(random(plants.length))
+    console.log(plants[randomIndex].name);
+    text(plants[randomIndex].name, 50, 50);
+    plants.splice(randomIndex, 1);
+    console.log(plants);
+  } else {
+    background(random(255), random(255), random(255));
+    text("nothing left!", 50, 50)
+  }
 
-function mousePressed(){
-  background(random(200 ,255));
-  randomIndex = int(random(plants.length))
-  console.log(plants[randomIndex].name);
-  text(plants[randomIndex].name, 50,50);
-  plants.splice(randomIndex, 1);
-  console.log(plants);
+}
+function mousePressed() {
+  animating = true
+  setTimeout(randomizer, 2000);
 
 }
