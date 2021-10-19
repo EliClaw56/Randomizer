3b178530f671704e47d791aa46208f824b0cf90c
EliClaw56 <89602283+EliClaw56@users.noreply.github.com>
2021-10-18 23:57:12 -0700

    update

--- sketch.js
+++ sketch.js
@@ -18,8 +18,8 @@ let cnv;
 // let counter = 0;
 
 function setup(){
-  // cnv = createCanvas(600, 450);
-  // cnv.parent("#canvasDiv")
+  cnv = createCanvas(600, 450);
+  cnv.parent("#canvasDiv")
   createCanvas(900, 450);
   angleMode(DEGREES);
   rectMode(CENTER);
@@ -27,7 +27,7 @@ function setup(){
   text(0,0,0);
 
   button = createButton("Generate Pick-up Line");
-  // button = select('#genButton')
+  button = select('#genButton')
   button.position(190, 280);
   button.mousePressed(button);
   button.class("randomizerButton");
@@ -37,6 +37,161 @@ function setup(){
 function draw() {
 
     }
+    function avatar(){
+      push();
+      strokeWeight(8)
+      stroke(53, 189, 204);
+      fill(148, 219, 227);
+      ellipse(700,450,240,350)
+
+      strokeWeight(8)
+      stroke(247,207,172);
+      fill(245,218,194);
+      ellipse(700,200,200,220)
+
+      strokeWeight(3)
+      stroke(247,207,172);
+      fill(255,255,255);
+      ellipse(650,200,60,60)
+
+      strokeWeight(3)
+      stroke(247,207,172);
+      fill(255,255,255);
+      ellipse(750,200,60,60)
+
+      noStroke();
+      fill(0,0,0);
+      ellipse(750,200,25,25)
+
+      noStroke();
+      fill(0,0,0);
+      ellipse(650,200,25,25)
+
+      noStroke();
+      fill(245,218,194);
+      rectMode(CENTER);
+      rect(650,183,70,30)
+
+      noStroke();
+      fill(245,218,194);
+      rectMode(CENTER);
+      rect(750,183,70,30)
+
+      //Mouth
+      noFill();
+      stroke(222,179,142);
+      arc(700,250,60,60,30,150);
+
+      //Nose
+      noFill();
+      stroke(222,179,142);
+      arc(700,210,60,60,70,110);
+
+      //LBrow
+      noFill();
+      strokeWeight(10);
+      stroke(128,83,44);
+      arc(650,210,60,60,240,300);
+
+      //RBrow
+      noFill();
+      strokeWeight(10);
+      stroke(128,83,44);
+      arc(750,190,60,60,240,300);
+
+      //Hat
+      fill(161,158,255);
+      strokeWeight(10);
+      stroke(92,87,255);
+      arc(700,150,190,180,180,360);
+
+      line(795,150,550,150);
+
+      push();
+      stroke(245,255,255);
+      noFill();
+      rect(280,200,520,80,20);
+      triangle(490,240,535,235,560,260);
+      pop();
+    }
+
+      function sadAvatar(){
+      strokeWeight(8)
+      stroke(53, 189, 204);
+      fill(148, 219, 227);
+      ellipse(700,450,240,350)
+
+      strokeWeight(8)
+      stroke(247,207,172);
+      fill(245,218,194);
+      ellipse(700,200,200,220)
+
+      strokeWeight(3)
+      stroke(247,207,172);
+      fill(255,255,255);
+      ellipse(650,200,60,60)
+
+      strokeWeight(3)
+      stroke(247,207,172);
+      fill(255,255,255);
+      ellipse(750,200,60,60)
+
+      noStroke();
+      fill(0,0,0);
+      ellipse(765,200,25,25)
+
+      noStroke();
+      fill(0,0,0);
+      ellipse(665,200,25,25)
+
+      noStroke();
+      fill(245,218,194);
+      rectMode(CENTER);
+      rect(650,170,70,30)
+
+      noStroke();
+      fill(245,218,194);
+      rectMode(CENTER);
+      rect(750,170,70,30)
+
+      //Mouth
+      noFill();
+      stroke(222,179,142);
+      arc(700,300,60,60,220,320);
+
+      //Nose
+      noFill();
+      stroke(222,179,142);
+      arc(700,210,60,60,70,110);
+
+      //LBrow
+      noFill();
+      strokeWeight(10);
+      stroke(128,83,44);
+      arc(650,195,60,60,240,300);
+
+      //RBrow
+      noFill();
+      strokeWeight(10);
+      stroke(128,83,44);
+      arc(750,195,60,60,240,300);
+
+      //Hat
+      fill(161,158,255);
+      strokeWeight(10);
+      stroke(92,87,255);
+      arc(700,150,190,180,180,360);
+
+      line(795,150,550,150);
+
+      push();
+      stroke(245,255,255);
+      noFill();
+      rect(280,200,520,80,20);
+      triangle(490,240,535,235,560,260);
+      pop();
+      }
+
 
 function randomizer(){
   animating = false;
@@ -59,157 +214,4 @@ function randomizer(){
 function mousePressed() {
     animating = true;
     setTimeout(randomizer, 1000);
-  }
-
-function avatar(){
-  push();
-  strokeWeight(8)
-  stroke(53, 189, 204);
-  fill(148, 219, 227);
-  ellipse(700,450,240,350)
-
-  strokeWeight(8)
-  stroke(247,207,172);
-  fill(245,218,194);
-  ellipse(700,200,200,220)
-
-  strokeWeight(3)
-  stroke(247,207,172);
-  fill(255,255,255);
-  ellipse(650,200,60,60)
-
-  strokeWeight(3)
-  stroke(247,207,172);
-  fill(255,255,255);
-  ellipse(750,200,60,60)
-
-  noStroke();
-  fill(0,0,0);
-  ellipse(750,200,25,25)
-
-  noStroke();
-  fill(0,0,0);
-  ellipse(650,200,25,25)
-
-  noStroke();
-  fill(245,218,194);
-  rectMode(CENTER);
-  rect(650,183,70,30)
-
-  noStroke();
-  fill(245,218,194);
-  rectMode(CENTER);
-  rect(750,183,70,30)
-
-  //Mouth
-  noFill();
-  stroke(222,179,142);
-  arc(700,250,60,60,30,150);
-
-  //Nose
-  noFill();
-  stroke(222,179,142);
-  arc(700,210,60,60,70,110);
-
-  //LBrow
-  noFill();
-  strokeWeight(10);
-  stroke(128,83,44);
-  arc(650,210,60,60,240,300);
-
-  //RBrow
-  noFill();
-  strokeWeight(10);
-  stroke(128,83,44);
-  arc(750,190,60,60,240,300);
-
-  //Hat
-  fill(161,158,255);
-  strokeWeight(10);
-  stroke(92,87,255);
-  arc(700,150,190,180,180,360);
-
-  line(795,150,550,150);
-
-  stroke(245,255,255);
-  noFill();
-  rect(280,200,520,80,20);
-  triangle(490,240,535,235,560,260);
-  pop();
-}
-
-  function sadAvatar(){
-  strokeWeight(8)
-  stroke(53, 189, 204);
-  fill(148, 219, 227);
-  ellipse(700,450,240,350)
-
-  strokeWeight(8)
-  stroke(247,207,172);
-  fill(245,218,194);
-  ellipse(700,200,200,220)
-
-  strokeWeight(3)
-  stroke(247,207,172);
-  fill(255,255,255);
-  ellipse(650,200,60,60)
-
-  strokeWeight(3)
-  stroke(247,207,172);
-  fill(255,255,255);
-  ellipse(750,200,60,60)
-
-  noStroke();
-  fill(0,0,0);
-  ellipse(765,200,25,25)
-
-  noStroke();
-  fill(0,0,0);
-  ellipse(665,200,25,25)
-
-  noStroke();
-  fill(245,218,194);
-  rectMode(CENTER);
-  rect(650,170,70,30)
-
-  noStroke();
-  fill(245,218,194);
-  rectMode(CENTER);
-  rect(750,170,70,30)
-
-  //Mouth
-  noFill();
-  stroke(222,179,142);
-  arc(700,300,60,60,220,320);
-
-  //Nose
-  noFill();
-  stroke(222,179,142);
-  arc(700,210,60,60,70,110);
-
-  //LBrow
-  noFill();
-  strokeWeight(10);
-  stroke(128,83,44);
-  arc(650,195,60,60,240,300);
-
-  //RBrow
-  noFill();
-  strokeWeight(10);
-  stroke(128,83,44);
-  arc(750,195,60,60,240,300);
-
-  //Hat
-  fill(161,158,255);
-  strokeWeight(10);
-  stroke(92,87,255);
-  arc(700,150,190,180,180,360);
-
-  line(795,150,550,150);
-
-  stroke(245,255,255);
-  noFill();
-  rect(280,200,520,80,20);
-  triangle(490,240,535,235,560,260);
-  pop();
   }
